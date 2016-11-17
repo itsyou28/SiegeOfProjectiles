d4d0178448e4275df7d09d861e57c82fc3efb9c6
itsyou28 <itsyou28@gmail.com>
2016-11-18 02:22:09 +0900

    - 타워 테스트 이미지 적용 - 타워 구현 방안 고민중 - 작업을 별로 진행하지 못함.
    p10 #72
    
    Signed-off-by: itsyou28 <itsyou28@gmail.com>

---
+++ Assets/Script/GamePlay/Tower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour
+{
+    //타워 HP 관리
+    //타워 애니메이션 관리
+    //접촉 적 공격 처리
+
+
+    void OnTriggerStay(Collider col)
+    {
+        //접촉중인 적군 처리
+    }
+}
